a8eef0bf788cd4cba02e980bb5426fde43efedfc
Lelouch Lamperouge <31546491+EvanRichard2@users.noreply.github.com>
2017-10-31 17:02:12 -0700

    Add files via upload

---
+++ Anime_Girl.bat
@@ -0,0 +1,67 @@
+@echo off
+title Anime Girl
+color 0C
+echo                                ``...........`                                           
+echo                                      `.-:///++++++++syys+:.                                        
+echo                                   `.-://++osyyhhhyyhmmmddy+/:.``.-.                                
+echo                                 `---/+syyysoo+++++++ohdddddyssyhddd:                               
+echo                                ---/osso+oo++o++++++++hdddddmmmddddh`                               
+echo                              `-.:oso++++++++++++++++smdmmmmshmddddh                                
+echo                             `-..:/+++++++++++++++o+omNNNmmsoodmmdmm-                               
+echo                             -://///++//++++++++s+yooshdddy++/+dmNNNo`                              
+echo                            -/++++++o+++oo+++++syohyso+oshy++//hhhhho/                              
+echo                           `/o+++++s+o+oy+++s+ohss+osys+++soo+++++yys+.                             
+echo                           .oo++++osoo+yo+ososs+oo::+syso++ss+++++syyo/                             
+echo                           -so+o++yssoyyoossoo/:o:---:+ooooss+++++yyyso`                            
+echo                           :ys+o+oysssysooo+/--//-::::::/ooos+o++oyyyys:                            
+echo                           -hsos+oyysoo+++/-..-:...-----.--/++s++syyyyy+                            
+echo                           .hsssooyss:+/:-..`.-````.-:+oss+/:+s++yyyyyys.                           
+echo                            yyssysyoo+soo/:.`.`````.:hddhdo:sososyyyyyhy+                           
+echo                            +yyyyysss:shhyy-`````````+osoo:`+oosyyyyyyyhy-                          
+echo                            .ysyyyss- ++///.         .:--:`-ooosyyoohyyyhy.                         
+echo                            oysyyys/`.-::-``   `   ```...-:++oyyyo+hhyyyhs.                        
+echo                             +hyyyyys/....```   `   ````....++syyyyhyyhyyyyy-                       
+echo                          `  oydyyyyss-..````    ``` ``````-o+yhyyydhyyhyyyyy/`                     
+echo                        ``` :yyhhsyyyss.``     `----`   `.od++shyyydhhyyyyyyyys:`                   
+echo                      ```  :syyyddyyyydds/.``   ```` ``-+yyh+++yhyhddyyyyyyyyyyso:`                 
+echo                     ``  .+osyyydhsssyhdhyo:::-.````.://::::+++oyhh+osssyyyyyyyssss/`               
+echo                       ./+--syyydysosys:------:++:-:::------+++++sh/```.:osyyyyysssss/.             
+echo                    `:+/- .oyyyyhss+oys---------::----------+o++++sy/...` /yyyyyysssoso/.           
+echo                   .o/.  -oyyyyyys++syo::--::::--------:::::/os++++oyo-.-::yyyyyyysss++oo/-         
+echo                   os   :osyyyyys+++sy/::-....---------------/ss+++++ss//:/yyyyyyyssso+:oo+/.       
+echo                   +s-.+ssyyyyys++++yo-- ```` ```.....```````.+sso++++syo-syyyyyyssssso+-so++-      
+echo                   .sysssyyyyys++++ss-.`           `` ```    `./ssoo+++sosysssssyyssssso:.oo+o-     
+echo                   -yyssyyyyyoo+++ss/:-`  `    ``  `.`..`````  `-osoo++ossyysssssssssssoo`.o+++     
+echo                  `syssyyyssoso++so:-`````..```````--:+:`````` `+-:oooo+sssssssssssssssso- -o+o`    
+echo                  .yyssssyssss+osyo-``   ``-````` `./s+-`     `.-``-ooo+osssssssssssooooo- `o++`    
+echo                  .ssssssyssosooyso+``      ``   ``.:..-`    ```.` .-ooooossooooooooooooo.  :o:     
+echo                  +ssssoossoooosoooo+`       ```   ``//-   ``.  `  `-/ooooooooooooooooooo`  :+      
+echo                  oooooooooo+ooo++++y+.       .``   `///.   ``     `.so+os+++++++++++++o:   +-      
+echo                  /++++++++++/oo++++y+o.     ``    `-///:`    `    ./y++/s+////++//+//+:    +-      
+echo                  `///////+/::/+++++s/oo.`  ``  ```.:////.`    ```.:yo:::s/::::/+////+-     .o.     
+echo                   :/:::/+:::::::::+:++:-.`.````.../++++-.`   ``.-o++:::o::::::/+::/-       `:.    
+echo                     ::::/+----:-----//+::-.-......-ooooo/..```..:+so+///+-----::+/:/               
+echo               `     ./--//----:---.-//o/::-.......ossssss:.....---+oo++//:------/.:/-`             
+echo              `::.``.:-:://-----....:/++//:::-...-+ssssssso/...::.`./oo+++/:----/.  `.`             
+echo                .::---.`` .:-..``..://///+:-:/:-:++ooossssoo+:/o.```./oo+++/:--::`                  
+echo                       ``...`````.:///.//+/-..-/+++++oooooooooo:.````./o+++//:..---`                
+echo                    ``..````````.:///-.///+/.``.-/+++++ooooooo/...````./+++///:....--`              
+echo                  `.``  ```````.:///.``//////.```.-/+++++oooo+...``````./+/////:...`.::.``          
+echo                 .`      ``````:///.```/////::.````..:+++++++-..````````.:+/////-.````::....`       
+echo                 `.       ````-//:.````:////--.``````.-/++++:...`````````.//////:.`````::` `-       
+echo                  -       ```-//:.````.:////-..````````:/++:....``````````.//////.```` `:-`.`       
+echo                  `-     ```-//:.`````.::///:...```````.///....````````````./////.```   -:--        
+echo                   ..   ```-//-.``````-::///:``````````.//....``````````````.////.``    `//`        
+echo                    ..````-//-.```````-::////`````````.::-```````````````````-///-      .-`         
+echo                     `.``-//:.```````.:::////.````````.--.```````````````````.///-   `..`           
+echo                       .:///:-...````.::::/:/.````````...`` ```````````..-://////- `-/:             
+echo                        -+//////::--`-:::::::-      ``````        ``.-::/++++////::++.              
+echo                         .////+:-::::::::::::-                  `.-:::::///://////:.                
+echo                          `:/:/------::::::::-          ```..---:::::::.:                           
+echo                            ` :......---:::/::``....--::/:-------------..                           
+echo                             .-........-----:. ```.......:---..-------.-                            
+echo                             -`.............-            -.............-                            
+echo                             -````````.....-`           `-..``..........                            
+echo                            `.````````````..            `-..```.......-                             
+echo                            -`````````````-             `.``````````.`-
+pause >null
