74f3f9d454c700356e8af80156858c5c94e2adb4
Lelouch Lamperouge <31546491+EvanRichard2@users.noreply.github.com>
2017-11-30 21:37:11 -0800

    Add files via upload

---
+++ Anime Cat.bat
@@ -0,0 +1,65 @@
+@echo off
+title Anime Cat
+color 05                                       
+echo                       ```                     `.--:///////::-.`.:+ssyhyysoo+//                      
+echo                   ``..``--.`.-:+osyyys++/.:+syyyhyyhyyhhyyyyyyhyhyhyhy+/+/////                      
+echo               ``.``.//--/::+yyyyyyyyy+osyhdhyyyhhyyhyyhhyyssyyyyys+:-..//////                      
+echo           `..``.``.:``+yhyyyyyyys++syoyyyhhhyyyyysyysyyysyyyyyyy+...``.-:/://                      
+echo         `.```.``-.`-..::::-`.o+:-..--+yyhhyyyyyyyyyyyyyyyyyyyyyy+`````.-/+/++                      
+echo         -``...-``:`./`````  `/-````..:yhhyyyyyyyyyyyyyyyyyyyhhhyy+..`..-+o+oo                      
+echo         .`-+.``:`.//-```    -o:--``.+hhyyyyyyyyyyyyyyyyyyyyyyhdhyyso:://+ooyo                      
+echo        .``::-`.::::--.     `shysso+yhhyyyyyyyyyyyyyyyyyhyyyyyyhdyyyys++syyyyy-                     
+echo        .```....-----`      +yyyyyyyhdyyyyyyyyyyyyyyyyyydhyhyyyyhhyyyyyyyyyyysy`                    
+echo       .```````./--.       -yossyyyhhyyyyyyyyyyyyyyyyyyydhydhyysshhsshyyyyyyyyyo                    
+echo       o``````.:--.       `+ooossyyhyyssssssyyyyyysyyssyhsyhhyyosshyshhyyyyyyhyh-                   
+echo      -hy+::+sh-         `.s++/o+yyhyooososssyyhyyosyysshs/hhhhs++shyhhosoossyyyy                   
+echo      odhhhhhhy          `:o+++++yhyhosssosssyhhhyoshyysho:/hhhhsoshhyys+/os+oooh-                  
+echo      yhhhhhhh:         ` /osyyooyhhysshhsssydhyhyshhyhyh/:-+hhhyyshhyyhoossoooooo                  
+echo     `hhhhhhhy         `` oyyhysshyhysyhhyyhdhhyhyyhyhhdh:/--shyhyyyhyyhhyyssyyyoh`                 
+echo     -hhhhhhho         ` `yyhhyyhhhhhyhhhyddhhhyhyhyyyhhs-:---yyyyyyhyyyhyyyyyhhhh.                 
+echo     /hhhhhhh/        `` -hhdhyhhhddhhhhddddhyhyhyyyyyhy+-----/yyhyyhyyyhhyhhyhddh/                 
+echo     yhhhhhhd/        .  /hdhhhdhhddyhhhdddhhhysyyosssss--:....+syyyhyyyhhhhdhhddyo                 
+echo    .dhhhhhhd/        `  +hdhhhdhhdhyhhhdhhyyossso/ooso+......-:ssyshsyyyhhhdhhdd+o.                
+echo    /hhhhhhhd+       .   +hdhhhhhyhhyhyhhoyyo-ss+./oo:o-.-/+shdddyssyssyyyyhdhhdd-o.                
+echo    shhhhhhhd+       .   /ydhhhhhyhyyyyhhyyhyoy/.`+o+:/.`.syoyhhyyyoysssyyyyhhhdd.+.                
+echo    hhhhhhhhdo       .   :yddyyhyyyyyys:+yhhddm/`.o+.:.``.:-osdhyyhsyssssyyyhhddd./``               
+echo   .hhhhhhhhd+       .   -ydyhyyyyysyso/yoyhhyd/.:+-`.`````.yysyo/sysosssyyyhhddd:. .               
+echo   +hhhhhhhhdo       .   `od/hyyysssss/-yh+hhso:`/.`````````:+::-/-oosssssyyhdddm/  .               
+echo   yhhhhhhhhdy        .   -h.yyyysssys.`-+/:::/```````````````````:-/sysysyhddhdm/  .               
+echo  `hhhhhhhhhdh        .   `s.:yyysssyo.````.`````````````````````.-`-shsysshddhhd:  -               
+echo  :hhhhhhhhhdh         .   -:`ssssssho/.````````````````````````..``:hdyyssyddhhy:  -               
+echo  ohhhhhhhhhdy          `` `+ :ysyssyyso.``````````````````````````:hddyysshddhh:o  -               
+echo  yhhhhhhhhhhs           `` :.`ysyssyyhh+.```````````````````````.+hhdhyysydhhhd.+- -               
+echo `hhhhhhhhhdyy`            `.:`sysyyyydhhy+:.``````````````````-+hhhdhdhyshdhhhd`.o`.               
+echo -hhhhhhhhhyhdys/:.``  ```   `-oysyyyydhhyhdhyo/-..````````.-//hhhhddhhhssddhhhd. //                
+echo /hhhhhhdhhhddddddy.--:/-`..```oyyoyohddhyyhdmmmdhyo+:---:/+/::hyhhddhss+yddhhhh. `o`               
+echo +hhhhhddhhhhhddddd.```.:``. .-/+sssshddhhhhddh+/+/o/////:----/+//sdddyo+hddhhhh- `:/               
+echo +hhhhhhhhhhhhhhddd.````.- .   .-:yhhddddhhdy:`````:/:-------//..`-odmhyydmdhhhh/ . :-              
+echo .+syhhhhhhhhhhhhho``````-  .   ..oyhdmhyyyh.      `./:-----:.    `-+dhhhmmdhhhh+``  /.             
+echo    .-:+osyhhhhhho-.`````-  .    `.yhdddyyyh/         .----:`      ::shhhdmddhhhs.    :`            
+echo         `-:+sys+--.....`-  .      +hhmhyyyh+`        `-::/--.`    :+ohhdyd+/oyyh      -`           
+echo             ``.-::---...:` .      `shdhsssy-:     `..-`.:--.---`  /:/dydhhs  `-+-``    ``          
+echo                 ..--::-:/` .       /yddsssy--. `...`   `-``` `.:-.. :hhhhyy/...--:-`     `         
+echo                 .``..-:/--..      `-sddsssy- `..`       -``.   ```  :+hhyssy:-.` ``.               
+echo                 .``..-/:--:-`     -`/ddsssy-            .. .        --yhyssy/      `               
+echo                  .`..::-.:-.--.``.: `hdsssy-             :..        `:-hyysss       .              
+echo                   .--------...----:  yhsssy-       ``..` --.         -`/yyssy.      -              
+echo                    ````...-so/::---  yysssy-             ` .          : osssss      :`             
+echo                           :hdhhhyo`  yso++o-             ` .          `.-oo++y.     -`             
+echo                           +hdhhddy   syssos:             ` .           `.sosos/     -`             
+echo                          `hydhhdho   sssssy/             . .             ssssys     `-             
+echo                          /hsdhyhh+   sssssy/             . -             +sssyy`    `..            
+echo                         `yysdyyhh+   sssssh/             . -             /sssys:      -            
+echo                         /hysdyyhho   ssssyh+             . .             :sssyo+`     -.           
+echo                         hyyyhyyhhy-. ssssyh+             . .             .yyyys+-     `:           
+echo                        :dyyhyyyhhh-:.sssshy+         ... `.. `.          `yyyyy/:`     -.          
+echo                        yhyyhyyydhh/::yhyyhy+     `..----`.+-`---.``      .yyyyh/--    `.-`         
+echo                       .dhyyyyyydhh+--syyyyyo `..---...-. .-/--....-...``.-syyyh/--- ``` .-         
+echo                       /dhyyshsyhhhs--oyyyyys-........... :.:..............syyyh/----.--:`          
+echo                       odhyoshsyhhhy:.oyyyyyy...........`-:.:...````.......syyyh/-----/.            
+echo                       odhy/yhyshhhh/.+yyyyyy.`````....`..:.-.````````.....syyyy/--://+             
+echo                       ohhy-yyysyhhho./yyyyyy.``````..``..:`:-``````````..-syyyyo///:.-.            
+echo                       /hyy.syyyhyhhy-:yyysyh-``````.``...:`::`````````...-ysssy+:.````-            
+echo                       .yyy`+yyydyyhd/-hyssyh:.````.. ....:`::.```````....-ysysy:.`````-            
+echo                        +yy`:yyyhhhyho-syysyy+-.....``....:-::-````.......:ysssy:-```````           
+pause > 0
